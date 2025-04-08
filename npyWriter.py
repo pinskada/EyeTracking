c97c76be78246d1623b6c4740613f4d8c0b9be2c
Adam Pinkser <apinser798@gmail.com>
2025-04-08 18:43:15 +0200

    MAINT: Update .gitignore.

--- npyWriter.py
+++ npyWriter.py
@@ -2,10 +2,10 @@ import numpy as np
 
 # Define the same dictionary
 data = {
-    'pupil': [41.0, (53, 53)],
+    'pupil': [40.6, (13, 13)],
     'cr1': [171.7, (3, 3)],
     'cr2': [171.7, (3, 3)]
 }
 
 # Save it as a .npy file with pickle enabled
-np.save('pupil_parameters.npy', data, allow_pickle=True)
+np.save('pupil_parameters_cropL.npy', data, allow_pickle=True)

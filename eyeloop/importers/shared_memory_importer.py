f3fb4cbb7efdbfcf3947dc4b8be449495a62aa73
API <a.pinsker@era.aero>
2025-10-31 09:35:05 +0100

    NEWFEAT: Redesign importeru.

--- eyeloop/importers/shared_memory_importer.py
+++ eyeloop/importers/shared_memory_importer.py
@@ -1,78 +1,85 @@
+"""Shared Memory Importer for Eyeloop Module."""
+
 from multiprocessing.shared_memory import SharedMemory
-from queue import Empty
+from threading import Event
+
 import numpy as np
-import cv2
-import time
-import threading
+
 import eyeloop.config as config
-from eyeloop.importers.importer import IMPORTER
+
 
 class Importer():
+    """Shared Memory Importer for Eyeloop Module."""
+
     def __init__(self) -> None:
         self.scale = config.arguments.scale
-        
+
         # Wait for init command via command_queue
         self.shared_memory_name = config.arguments.sharedmem
         self.side = config.arguments.side
-        self.frame_shape = None
-        self.frame_dtype = None
+        self.frame_shape: tuple[int, int]
+        self.frame_dtype: np.dtype
         self.frame = None
         self.print_status = False
-        self.current_frame_id = 0
 
         self.command_queue = config.command_queue
-        self.sync_queue = config.sync_queue
+        self.tracker_shm_is_closed_signal = config.tracker_shm_is_closed_signal
 
-        self._initialize_from_command_queue()
+        self.tracker_shm_is_closed_signal.set()
 
-    def _initialize_from_command_queue(self):
-        print(f"[INFO] Importer {self.side}: Waiting for init command...")
-        while True:
-            try:
-                msg = self.command_queue.get(timeout=1)
-                if msg.get("type") == "memory":
-                    self._setup_shared_memory(msg)
-                    break
-            except Empty:
-                
-                continue
+        self.set_stop_event = Event()
+        self.new_frame_event = Event()
 
-    def route(self):
-        self.first_frame()
-        time.sleep(0.3)  # Wait for the engine to stabilize
-        print(f"[INFO] Importer {self.side}: Starting routing thread...\n")
-        self.proceed_thread = threading.Thread(target=self.proceed())
-        self.proceed_thread.run() # Start the frame provider
-        
-    def proceed(self) -> None:
-        while True:
-            self.load_command_queue()
+        self.shm: SharedMemory
 
-            if self.shm is None:
-                while self.shm is None:
-                    self.load_command_queue()
-                    time.sleep(0.001)
 
-            if self.current_frame_id % 50 == 0 and self.print_status:
-                print(f"[INFO] Importer {self.side}: Current frame ID: {self.current_frame_id}\n")
-        
-            try:
-                msg = self.sync_queue.get(timeout=1)
-                if msg.get("type") == 'frame_id':
-                    self.print_status = True
-                    self.current_frame_id = msg.get('frame_id')
-                    frame = self.route_frame()
-                    config.engine.iterate(frame)
-            except Empty:
-                self.print_status = False
-                time.sleep(0.001)
+    def route(self) -> None:
+        """Start routing frames from shared memory to the engine."""
 
-    def first_frame(self) -> None:
-        print(f"[INFO] Importer {self.side}: First frame processing started.\n")
+        self._load_cmd_queue_message(timeout=10.0)
+
+        self._first_frame()
+        print(f"[INFO] Importer {self.side}: Starting routing...\n")
+        self._proceed()
+
+
+    def _load_cmd_queue_message(self, timeout: float = 5.0) -> None:
+        """Loads a message from the command queue."""
 
         try:
-            frame = self.route_frame()
-        except Exception as e:
+            if not (msg := self.command_queue.get(timeout=timeout)):
+                print(f"[ERROR] Importer {self.side}: No message received from command queue.")
+                return
+
+            if self.tracker_shm_is_closed_signal.is_set() and msg.get("type") != "shm_connect":
+                print(f"[WARN] Importer {self.side}: First message must be 'shm_connect'.")
+                return
+
+            if msg.get("type") == "shm_connect":
+                self._connect_shm(msg)
+            elif msg.get("type") == "shm_detach":
+                self._close_shm()
+            elif msg.get("type") == "close":
+                print(f"[INFO] Importer {self.side}: Closing shared memory...")
+                self.set_stop_event.set()
+                config.engine.release()
+            elif msg.get("type") == "config":
+                self._configure(msg)
+            elif msg.get("type") == "frame_id":
+                config.current_frame_id = msg.get("frame_id")
+                self.new_frame_event.set()
+            else:
+                print(f"[INFO] Importer {self.side}: Unknown command: {msg.get('type')}")
+        except Exception:  # pylint: disable=broad-except
+            pass
+
+
+    def _first_frame(self) -> None:
+        """Process the first frame to arm the engine."""
+
+        try:
+            frame = self._get_frame()
+        except Exception as e:  # pylint: disable=broad-except
             print(f"[ERROR] Importer {self.side}: Error in first frame processing: {e}")
             return
 
@@ -81,54 +88,61 @@ class Importer():
             height=self.frame_shape[0],
             width=self.frame_shape[1],
             image=frame
-        )   
+        )
 
-    def route_frame(self) -> None:
+
+    def _get_frame(self) -> np.ndarray:
+        """Get a frame from the Shared Memory."""
+
         return np.ndarray(self.frame_shape, dtype=self.frame_dtype, buffer=self.shm.buf).copy()
 
-    def load_command_queue(self):
+
+    def _proceed(self) -> None:
+        """Main loop to route frames from shared memory to the engine."""
+
+        while not self.set_stop_event.is_set():
+
+            # Check for new commands
+            self._load_cmd_queue_message()
+
+            # Skip processing if shared memory is closed
+            if self.tracker_shm_is_closed_signal.is_set():
+                continue
+
+            # print status every 50 frames
+            if config.current_frame_id % 50 == 0 and self.print_status:
+                print(f"[INFO] Importer {self.side}: Current frame ID: {config.current_frame_id}\n")
+
+            # Skip if no new frame is yet available
+            if self.new_frame_event.is_set():
+                # Get frame from shared memory and iterate
+                frame = self._get_frame()
+                config.engine.iterate(frame)
+                self.new_frame_event.clear()
+
+
+    def _configure(self, msg):
+        """Configure the run-time parameters based on the received message."""
+
         try:
-            msg = self.command_queue.get_nowait()
+            if  msg.get("param") == "threshold":
+                value = msg.get("value")
+                config.graphical_user_interface.pupil_processor.binarythreshold = value
+                print(f"[INFO] Importer {self.side}: Threshold decreased to "
+                    f"{config.graphical_user_interface.pupil_processor.binarythreshold}."
+                )
 
-            if msg.get("type") == "memory":
-                self._setup_shared_memory(msg)
-            elif msg.get("type") == "detach":
-                self.close_memory()
-            elif msg.get("type") == "close":
-                print(f"[INFO] Importer {self.side}: Closing shared memory...")
-                config.engine.release()
-            elif msg.get("type") == "config":
-                self.configure(msg)
-            else:
-                print(f"[INFO] Importer {self.side}: Unknown command: {msg.get('type')}")
-        except Exception:
-            pass        
-
-    def configure(self, msg):
-        """ Configure the run-time parameters based on the received message."""
-
-        try:
-            if  msg.get("param") == "threshold_down":
-                config.graphical_user_interface.pupil_processor.binarythreshold -= 1
-                print(f"[INFO] Importer {self.side}: Thresholsd decreased to {config.graphical_user_interface.pupil_processor.binarythreshold}.")
-
-            elif  msg.get("param") == "threshold_up":
-                config.graphical_user_interface.pupil_processor.binarythreshold += 1
-                print(f"[INFO] Importer {self.side}: Thresholsd increased to {config.graphical_user_interface.pupil_processor.binarythreshold}.")
-
-            elif msg.get("param") == "blur_down":
+            elif msg.get("param") == "blur":
                 blur = config.graphical_user_interface.pupil_processor.blur
                 if blur[0] >= 3 and blur[1] >= 3:
-                    config.graphical_user_interface.pupil_processor.blur = tuple(x-2 for x in blur)
-                    print(f"[INFO] Importer {self.side}: Blur idecreased to {config.graphical_user_interface.pupil_processor.blur}.")
+                    value = msg.get("value")
+                    config.graphical_user_interface.pupil_processor.blur = (value, value)
+                    print(f"[INFO] Importer {self.side}: "
+                        f"Blur decreased to {config.graphical_user_interface.pupil_processor.blur}."
+                    )
                 else:
                     print(f"[INFO] Importer {self.side}: Minimum blur reached.")
 
-            elif msg.get("param") == "blur_up":
-                blur = config.graphical_user_interface.pupil_processor.blur
-                config.graphical_user_interface.pupil_processor.blur = tuple(x+2 for x in blur)
-                print(f"[INFO] Importer {self.side}: Blur increased to {config.graphical_user_interface.pupil_processor.blur}.")
-    
             elif msg.get("param") == "auto_search":
                 config.arguments.auto_search = msg.get("value")
                 print(f"[INFO] Importer {self.side}: auto_search set to {msg.get('value')}")
@@ -148,25 +162,36 @@ class Importer():
                 config.preview = msg.get("value")
                 print(f"[INFO] Importer {self.side}: Preview set to {msg.get('value')}")
             else:
-                print(f"[INFO] Importer {self.side}: Unknown configuration parameter: {msg.get('param')}")
-        except Exception as e:
+                print(f"[INFO] Importer {self.side}: "
+                    f"Unknown configuration parameter: {msg.get('param')}"
+                )
+        except (TypeError, ValueError, KeyError) as e:
             print(f"[WARN] Importer {self.side}: Failed to apply config: {e}")
-    def _setup_shared_memory(self, msg):
+
+
+    def _connect_shm(self, msg):
+        """Setup the shared memory segment."""
 
         self.frame_shape = tuple(msg["frame_shape"])
         self.frame_dtype = np.dtype(msg["frame_dtype"])
         self.shm = SharedMemory(name=self.shared_memory_name)
-        print(f"[INFO] Importer {self.side}: Memory initialized with {self.shared_memory_name}, shape {self.frame_shape}, dtype {self.frame_dtype}\n")
+        self.tracker_shm_is_closed_signal.clear()
+        print(f"[INFO] Importer {self.side}: Shared memory initialized.")
 
-    def close_memory(self):
+
+    def _close_shm(self):
+        """Close the shared memory segment."""
         try:
             self.shm.close()
-            self.shm = None
-        except Exception as e:
+            self.tracker_shm_is_closed_signal.set()
+            print(f"[INFO] Importer {self.side}: Shared memory closed.")
+        except (FileNotFoundError, PermissionError, OSError, BufferError) as e:
             print(f"[ERROR] Importer {self.side}: Error closing shared memory: {e}")
 
+
     def release(self) -> None:
+        """Release the importer and shared memory."""
+
         print(f"[INFO] Importer {self.side}: cv.Importer.release() called")
-        self.close_memory()
+        self._close_shm()
         #cv2.destroyAllWindows()
-     

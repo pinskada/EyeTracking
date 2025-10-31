146d8eb82f575e8c364fe86ad5cfa8a7ecdbe2d3
API <a.pinsker@era.aero>
2025-10-31 09:34:45 +0100

    NEWFEAT: Redesign extractoru.

--- eyeloop/extractors/queue_extractor.py
+++ eyeloop/extractors/queue_extractor.py
@@ -1,47 +1,69 @@
+"""Queue extractor module for sending processed data via queues."""
+
+import numpy as np
+
 import eyeloop.config as config
-from eyeloop.utilities.encode_binary_float64_as_png import encode_binary_float64_as_png
-import time
+
+
 class QueueExtractor:
+    """Queue extractor for sending processed data via queues."""
+
     def __init__(self):
         self.response_queue = config.response_queue
-        self.acknowledge_queue = config.acknowledge_queue
         self.side = config.arguments.side
 
-    def activate(self):
-        print(f"[INFO] Extractor {self.side}: QueueExtractor activated.")
-    
-    def fetch(self, core):
-        if config.importer.current_frame_id != 0:
-            
-            if config.preview:
-                try:
-                    png_image = encode_binary_float64_as_png(config.graphical_user_interface.bin_P)
 
-                    config.response_queue.put(png_image)
-                except ValueError as e:
-                    print(f"[ERROR] Extractor {self.side}: QueueExtractor.fetch() ValueError: {e}")
-            
-            try:
-                message = {
-                    "type": "payload",
-                    "frame_id": config.importer.current_frame_id,
-                    "data": core.dataout
+    def activate(self):
+        """Activate the QueueExtractor."""
+        print(f"[INFO] Extractor {self.side}: QueueExtractor activated.")
+
+
+    def fetch(self, core):
+        """Fetch processed data and send it via queues."""
+        if config.importer != 0:
+
+            # Signal to FrameProvider to fetch the next frame
+            config.eye_ready_signal.set()
+
+            # Create message with tracking data
+            tracking_data_message = {
+                "type": "eye_data",
+                "frame_id": config.current_frame_id,
+                "data": core.dataout
+            }
+
+            # Send tracking data message via response queue
+            config.response_queue.put(tracking_data_message)
+
+            if config.preview:
+
+                # Create message with image preview data
+
+                image_preview = config.graphical_user_interface.bin_P
+
+                image_height = image_preview.shape[0]
+                image_width = image_preview.shape[1]
+
+                bit_map = np.packbits(image_preview.astype(np.uint8))
+
+                tracking_data_message = {
+                    "type": "image_preview",
+                    "frame_id": config.current_frame_id,
+                    "height": image_height,
+                    "width": image_width,
+                    "bitmap": bit_map
                 }
-                #print(f"[INFO] Extractor {self.side}: Sending response with frame ID: {config.importer.current_frame_id}")
-                config.response_queue.put(message)
-            except ValueError as e:
-                print(f"[ERROR] Extractor {self.side}: QueueExtractor.fetch() error: {e}")
-            time.sleep(0.005)
-            try:
-                self.acknowledge_queue.put({"type": "ack", "frame_id": config.importer.current_frame_id})
-                #print(f"[INFO] Extractor {self.side}: Acknowledged frame ID: {config.importer.current_frame_id}")
-            except ValueError as e:
-                print(f"[ERROR] Extractor {self.side}: Error writing to acknowledge queue: {e}")
-            
+
+                # Send image preview message via response queue
+                config.response_queue.put(tracking_data_message)
+
+
     def __name__(self):
+        """Return the name of the extractor."""
         return "QueueExtractor"
 
+
+    #  pylint: disable=unused-argument
     def release(self, core):
-        print("[INFO] Extractor {self.side}: QueueExtractor released called, passing.")
-        pass
-    
+        """Release resources held by the QueueExtractor."""
+        print(f"[INFO] Extractor {self.side}: QueueExtractor released called, passing.")
